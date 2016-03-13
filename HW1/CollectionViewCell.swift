a3c55960335afce4b4ae3383982bd2b5005629a2
Perry <perrchick@gmail.com>
2016-03-13 08:05:10 +0200

    Perry's suggestions
    Added runtime optimizations (removed some nested loops), changed naming, removed redundant code.

--- HW1/CollectionViewCell.swift
+++ HW1/CollectionViewCell.swift
@@ -12,53 +12,55 @@ class CollectionViewCell: UICollectionViewCell {
     var imageView: UIImageView!
     var backImageView: UIImageView!
     
-    private var image: UIImage?
-    var isFlipped = false
-    var isCardHidden = false
-    var canClick = true
-    
-    func SetImage(im: UIImage) {
-        image = im
-        if(imageView == nil) {
-            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
-            imageView.contentMode = UIViewContentMode.ScaleAspectFit
+    var image: UIImage? {
+        didSet {
+            if(imageView == nil) {
+                imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+                imageView.contentMode = UIViewContentMode.ScaleAspectFit
+            }
+            if(backImageView==nil) {
+                backImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+                backImageView.contentMode = UIViewContentMode.ScaleAspectFit
+                backImageView.image = UIImage(named: "back.png")
+                contentView.addSubview(backImageView)
+            }
+            
+            imageView.image = image
         }
-        if(backImageView==nil) {
-            backImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
-            backImageView.contentMode = UIViewContentMode.ScaleAspectFit
-            backImageView.image = UIImage(named: "back.png")
-            contentView.addSubview(backImageView)
-        }
-        
-        imageView.image = image
     }
+    private var isAnimating = false
+    private(set) var isFlipped = false
     
-    func FlipCard() {
-        if (!isFlipped) {
-            UIView.transitionFromView(backImageView, toView: imageView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+    func flipCard(animated: Bool = true) {
+        if animated {
+            isAnimating = true
+            if (!isFlipped) {
+                UIView.transitionFromView(backImageView, toView: imageView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight) { (done) in
+                    self.isAnimating = false
+                }
+            } else {
+                UIView.transitionFromView(imageView, toView: backImageView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft) { (done) in
+                    self.isAnimating = false
+                }
+            }
         } else {
-            UIView.transitionFromView(imageView, toView: backImageView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+            if (!isFlipped) {
+                backImageView.superview?.addSubview(imageView)
+                backImageView.removeFromSuperview()
+            } else {
+                imageView.superview?.addSubview(backImageView)
+                imageView.removeFromSuperview()
+            }
         }
+
         isFlipped = !isFlipped
     }
     
-    func HideCard() {
-        imageView.removeFromSuperview()
-        backImageView.removeFromSuperview()
-        imageView = nil
-        backImageView = nil
-        isCardHidden = true
-        isFlipped = false
-    }
-    
-    func CanClick() -> Bool {
-        return !isFlipped && canClick && !isCardHidden
+    func isClickable() -> Bool {
+        return !isFlipped && !self.hidden && !isAnimating
     }
     
     func resetCell() {
-        canClick = true
-        isCardHidden = false
-        isFlipped = false
-        SetImage(image!)
+        self.hidden = false
     }
 }
